daa17f45b3b3d5e68ea767fe6f3da94a75975225
Markus Werner <markus@echt-werner.de>
2024-03-31 20:14:30 +0200

    bug fix

--- kernel/main.c
+++ kernel/main.c
@@ -18,6 +18,7 @@ main()
     printf("xv6 kernel is booting\n");
     printf("\n");
     printf("Stacksize per CPU is %d bytes\n", 1024 * STACKSIZECPU);
+    printf("\n");
     kinit();         // physical page allocator
     kvminit();       // create kernel page table
     kvminithart();   // turn on paging
